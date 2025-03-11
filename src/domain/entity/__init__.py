80a50172fb06b5efde72ab2b4225d45d8ffb3e68
RobertoRuben <robertoch263@gmail.com>
2025-03-11 15:04:23 -0500

    ♻️ refactor(entity): update __init__.py to import DocumentaryDomain and adjust __all__ export

--- src/domain/entity/__init__.py
+++ src/domain/entity/__init__.py
@@ -1,7 +1,4 @@
-from .domain import Domain
+from .documentary_domain import DocumentaryDomain
 from .page import Page
 
-__all__ = [
-    "Domain",
-    "Page"
-]
+__all__ = ["DocumentaryDomain", "Page"]
