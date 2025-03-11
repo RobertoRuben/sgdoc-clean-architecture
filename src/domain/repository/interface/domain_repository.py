8a3b04bcda9c5c597a74c306af4699b1a7208b01
RobertoRuben <robertoch263@gmail.com>
2025-03-11 15:04:09 -0500

    ♻️ refactor(repository): update DomainRepository interface to use DocumentaryDomain instead of Domain

--- src/domain/repository/interface/domain_repository.py
+++ src/domain/repository/interface/domain_repository.py
@@ -1,69 +1,65 @@
 from abc import ABC, abstractmethod
-from src.domain.entity import Domain, Page
+from src.domain.entity import DocumentaryDomain, Page
+
 
 class DomainRepository(ABC):
     """
     Interface for the Domain repository
     """
-    
+
     @abstractmethod
-    async def add(self, domain: Domain) -> Domain:
+    async def add(self, domain: DocumentaryDomain) -> DocumentaryDomain:
         """
         Add a domain to the repository
-        
+
         :param domain: Domain object
         :return: Domain object
         """
         pass
-    
-    
+
     @abstractmethod
     async def get_paginated(self, page: int, size: int) -> Page:
         """
         Get paginated domains
-        
+
         :param page: Page number
         :param size: Page size
         :return: Page object
         """
         pass
-    
-    
+
     @abstractmethod
-    async def update(self, domain: Domain) -> Domain:
+    async def update(self, domain: DocumentaryDomain) -> DocumentaryDomain:
         """
         Update a domain in the repository
-        
+
         :param domain: Domain object
         :return: Domain object
         """
         pass
-    
-    
+
     @abstractmethod
     async def delete(self, domain_id: int) -> bool:
         """
         Delete a domain from the repository
-        
+
         :param domain_id: Domain ID
         :return: True if the domain was deleted, False otherwise
         """
         pass
-    
-    
+
     @abstractmethod
     async def find(self, page: int, size: int, search_dict: dict[str, str]) -> Page:
         """
         Find tenants based on partial matches in specified fields
-        
+
         :param page: Page number
         :param size: Items per page
         :param search_dict: Dictionary with field names as keys and search terms as values
         :return: Page object containing matching tenants
         """
         pass
-    
-    
+
     @abstractmethod
     async def get_by_id(self, tenant_id: int) -> Page:
         """
@@ -73,8 +69,7 @@ class DomainRepository(ABC):
         :return: Tenant object with the specified ID.
         """
         pass
-    
-    
+
     @abstractmethod
     async def exists_by(self, **kwargs) -> bool:
         """
